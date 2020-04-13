48e51852c05c2b3259a2c0f7ac802a6b6e6ded43
Jieun Lee <jieun-lee@users.noreply.github.com>
2020-04-12 21:40:18 -0700

    Add Drum Component

--- src/script.js
+++ src/script.js
@@ -1,8 +1,46 @@
-window.addEventListener('load', () => {
-    var app = new Vue({
-        el: '#app',
-        data: {
-            title: 'Mini Drum Kit'
+// returns the keycode for the given event
+var getDefaultKeyCode = function(type) {
+    switch(type) {
+        case "Kick":
+            return 69; // "e"
+            break;
+        case "Snare":
+            return 70; // "f"
+            break;
+        case "Hat":
+            return 74; // "j"
+            break;
+        case "Crash":
+            return 73; // "i"
+            break;
+        default:
+            return 32; // spacebar
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////
+
+Vue.component('drum', {
+    props: ['type'],
+    template: `
+        <div>
+        <h2>{{ name }}</h2>
+        <p>{{ file }}</p>
+        <p>{{ keycode }}</p>
+        </div>
+    `,
+    data() {
+        return {
+            name: this.type,
+            file: "/audio/" + this.type + ".wav",
+            keycode: getDefaultKeyCode(this.type),
         }
-    });
-})
+    }
+});
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        title: 'Mini Drum Kit'
+    }
+});
